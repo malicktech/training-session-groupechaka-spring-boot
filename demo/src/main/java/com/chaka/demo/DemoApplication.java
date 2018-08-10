b458b276b96f4e194569ac729833cd93b56468d5
Malick DIOP <diop-malick@hotmail.fr>
2018-08-10 12:33:28 +0000

    Service Web RESTful - Hello world ! - Refactoring

--- demo/src/main/java/com/chaka/demo/DemoApplication.java
+++ demo/src/main/java/com/chaka/demo/DemoApplication.java
@@ -9,10 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -20,11 +17,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String Hello() {
-		return "Hello, World!";
 	}
 
 	@Bean
