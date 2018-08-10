d303245fbb6515380431205e50b72301f9143a73
Malick DIOP <diop-malick@hotmail.fr>
2018-08-10 12:04:56 +0000

    Spring data JPA - Accés aux données

--- demo/src/main/java/com/chaka/demo/DemoStartupCommandLineRunner.java
+++ demo/src/main/java/com/chaka/demo/DemoStartupCommandLineRunner.java
@@ -1,11 +1,16 @@
 package com.chaka.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.chaka.demo.domain.User;
+import com.chaka.demo.repository.UserRepository;
 
 /**
  * Interface dans spring boot qui permet d'Exécuter du code au démarrage de
@@ -18,11 +23,48 @@ public class DemoStartupCommandLineRunner implements CommandLineRunner {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+	private UserRepository userRepository;
+
+	public DemoStartupCommandLineRunner(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("-------------------------------");
 		log.info("Application started with command-line arguments: {}", Arrays.toString(args));
 		log.info("-------------------------------\n ");
+
+		/**
+		 * Test Spring data japa
+		 */
+		
+		// save users
+		userRepository.save(new User("Jack", "Bauer", LocalDate.of(1950, Month.JANUARY, 1)));
+		userRepository.save(new User("Chloe", "O'Brian", LocalDate.of(1975, Month.MARCH, 1)));
+		userRepository.save(new User("Kim", "Bauer", LocalDate.of(1990, Month.DECEMBER, 1)));
+		userRepository.save(new User("David", "Palmer", LocalDate.of(2000, Month.AUGUST, 1)));
+		userRepository.save(new User("Michelle", "Dessler", LocalDate.of(2005, Month.JANUARY, 1)));
+
+		// fetch all users
+		log.info("User found with findAll():");
+		log.info("-------------------------------");
+		userRepository.findAll().forEach(u -> log.info(u.toString()));
+		log.info("");
+		
+		// fetch an individual user by ID
+		userRepository.findById(1L).ifPresent(customer -> {
+			log.info("User found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+		});
+
+		// fetch user by last name
+		log.info("Customer found with findByLastName('Bauer'):");
+		log.info("--------------------------------------------");
+		userRepository.findByLastName("Bauer").forEach(u -> log.info(u.toString()));
+		log.info("");
 	}
 
 }
---
+++ demo/src/main/java/com/chaka/demo/domain/User.java
@@ -0,0 +1,80 @@
+package com.chaka.demo.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private LocalDate birthdate;
+
+	public User() {
+		super();
+	}
+
+	public User(String firstName, String lastName, LocalDate birthdate) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthdate = birthdate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", birthdate=" + birthdate + "]";
+	}
+
+}
