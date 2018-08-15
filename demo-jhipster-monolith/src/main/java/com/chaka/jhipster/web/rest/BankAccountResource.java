91b2347be8bf4321005266b07ca65e39143d3b51
Malick DIOP <diop-malick@hotmail.fr>
2018-08-15 20:17:05 +0000

    Jhipster monolith | Un admin peut voir tout les comptes en banques

--- demo-jhipster-monolith/src/main/java/com/chaka/jhipster/web/rest/BankAccountResource.java
+++ demo-jhipster-monolith/src/main/java/com/chaka/jhipster/web/rest/BankAccountResource.java
@@ -1,12 +1,12 @@
 package com.chaka.jhipster.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import com.chaka.jhipster.domain.BankAccount;
-import com.chaka.jhipster.service.BankAccountService;
-import com.chaka.jhipster.web.rest.errors.BadRequestAlertException;
-import com.chaka.jhipster.web.rest.util.HeaderUtil;
-import com.chaka.jhipster.web.rest.util.PaginationUtil;
-import io.github.jhipster.web.util.ResponseUtil;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -14,14 +14,25 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
+import com.chaka.jhipster.domain.BankAccount;
+import com.chaka.jhipster.security.AuthoritiesConstants;
+import com.chaka.jhipster.security.SecurityUtils;
+import com.chaka.jhipster.service.BankAccountService;
+import com.chaka.jhipster.web.rest.errors.BadRequestAlertException;
+import com.chaka.jhipster.web.rest.util.HeaderUtil;
+import com.chaka.jhipster.web.rest.util.PaginationUtil;
+import com.codahale.metrics.annotation.Timed;
 
-import java.util.List;
-import java.util.Optional;
+import io.github.jhipster.web.util.ResponseUtil;
 
 /**
  * REST controller for managing BankAccount.
@@ -92,7 +103,14 @@ public class BankAccountResource {
     @Timed
     public ResponseEntity<List<BankAccount>> getAllBankAccounts(Pageable pageable) {
         log.debug("REST request to get a page of BankAccounts");
-        Page<BankAccount> page = bankAccountService.findAllUserBankAccount(pageable);
+        
+		Page<BankAccount> page;
+
+		if (SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
+			page = bankAccountService.findAll(pageable);
+		else
+			page = bankAccountService.findAllUserBankAccount(pageable);
+		
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/bank-accounts");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
